8de8a049b9438312c2381bdbf2333dfcfdc51c2b
xpoko <41261137+xpoko@users.noreply.github.com>
2019-03-13 15:18:48 +0700

    update misc

--- populate.sql
+++ populate.sql
@@ -1,15 +1,18 @@
 USE r69420;
 
+DELETE FROM `role`;
 INSERT INTO `role` (`role_name`) VALUES ('admin');
 INSERT INTO `role` (`role_name`) VALUES ('guest');
 INSERT INTO `role` (`role_name`) VALUES ('director');
 INSERT INTO `role` (`role_name`) VALUES ('manager');
 INSERT INTO `role` (`role_name`) VALUES ('student');
 
+DELETE FROM `faculty`;
 INSERT INTO `faculty` (`faculty_name`) VALUES ('computing');
 INSERT INTO `faculty` (`faculty_name`) VALUES ('business');
 INSERT INTO `faculty` (`faculty_name`) VALUES ('design');
 
+DELETE FROM `user`;
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('1', '1', 'admin@m.com', 'admin');
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('4', '1', 'mng1@m.com', 'mng1');
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('4', '2', 'mng2@m.com', 'mgn2');
@@ -22,6 +25,7 @@ INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('5'
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('5', '2', 'std2@m.com', 'std2');
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('5', '3', 'std3@m.com', 'std3');
 
+DELETE FROM `closure`;
 INSERT INTO `closure` (`academic_year`, `closure_date`) VALUES ('2017', '2017-11-11');
 INSERT INTO `closure` (`academic_year`, `closure_date`) VALUES ('2018', '2018-11-11');
 INSERT INTO `closure` (`academic_year`, `closure_date`) VALUES ('2019', '2019-11-11');
