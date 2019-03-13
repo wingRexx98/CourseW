fc65fc8fd6bfc2d50cdef49e48a0352c5568642f
xpoko <41261137+xpoko@users.noreply.github.com>
2019-03-13 15:25:17 +0700

    misc

--- populate.sql
+++ populate.sql
@@ -1,6 +1,9 @@
 USE r69420;
 
+SET SQL_SAFE_UPDATES = 0;
+
 DELETE FROM `role`;
+ALTER TABLE `role` AUTO_INCREMENT = 1;
 INSERT INTO `role` (`role_name`) VALUES ('admin');
 INSERT INTO `role` (`role_name`) VALUES ('guest');
 INSERT INTO `role` (`role_name`) VALUES ('director');
@@ -8,11 +11,13 @@ INSERT INTO `role` (`role_name`) VALUES ('manager');
 INSERT INTO `role` (`role_name`) VALUES ('student');
 
 DELETE FROM `faculty`;
+ALTER TABLE `faculty` AUTO_INCREMENT = 1;
 INSERT INTO `faculty` (`faculty_name`) VALUES ('computing');
 INSERT INTO `faculty` (`faculty_name`) VALUES ('business');
 INSERT INTO `faculty` (`faculty_name`) VALUES ('design');
 
 DELETE FROM `user`;
+ALTER TABLE `user` AUTO_INCREMENT = 1;
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('1', '1', 'admin@m.com', 'admin');
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('4', '1', 'mng1@m.com', 'mng1');
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('4', '2', 'mng2@m.com', 'mgn2');
@@ -26,6 +31,7 @@ INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('5'
 INSERT INTO `user` (`role_id`, `faculty_id`, `username`, `password`) VALUES ('5', '3', 'std3@m.com', 'std3');
 
 DELETE FROM `closure`;
+ALTER TABLE `closure` AUTO_INCREMENT = 1;
 INSERT INTO `closure` (`academic_year`, `closure_date`) VALUES ('2017', '2017-11-11');
 INSERT INTO `closure` (`academic_year`, `closure_date`) VALUES ('2018', '2018-11-11');
 INSERT INTO `closure` (`academic_year`, `closure_date`) VALUES ('2019', '2019-11-11');
