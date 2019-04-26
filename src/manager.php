4cb475cbbf7190959d0719b6f0951ece792eca4a
xpoko <41261137+xpoko@users.noreply.github.com>
2019-04-26 15:58:44 +0700

    1.3.1

--- src/manager.php
+++ src/manager.php
@@ -77,152 +77,153 @@ function getSubmission($conn, $facultyId)
     <!--jQuery-->
     <script src="js/jquery-3.3.1.min.js"></script>
     <title>
-        Home
+        Director Manager
     </title>
 </head>
-    <!--Default header-->
-        <div class="header-footer">
-            <div id="header" class="row justify-content-between">
-                <div class="col-auto">
-                    <h1><a style="color: #000000;
-    text-decoration: none;" href="index.php">News Management System</a></h1>
-                </div>
-                <div class="col-7">
-                    <h5><a style="color: #000000;
-    text-decoration: none;" href="logout.php">Logout</a></h5>
-                </div>
-            </div>
+<!--Default header-->
+<div class="header-footer">
+    <div id="header" class="row justify-content-between">
+        <div class="col-auto">
+            <a href="index.php">
+                <h1>News Management System</h1>
+            </a>
+        </div>
+        <span class="col-3">
+            <a class="btn btn-link" href="logout.php">
+                <span>Logout</span>
+            </a>
+        </span>
+    </div>
+</div>
+
+<!--body-->
+<div class="container">
+    <div id="body">
+        <div id="AdminTitle" class="row pt-3">
+
+            <label>
+                <h2 class="col">Manager Home Page</h2>
+            </label>
+
         </div>
 
-    <!--body-->
-    <div class="container">
-        <div id="body">
-            <div id="AdminTitle" class="row pt-3">
+        <div class="row">
+            <ul class="nav nav-tabs col-36">
+                <li class="nav-item">
+                    <a data-toggle="tab" class="nav-link" href="#com">Computing</a>
+                </li>
+                <li class="nav-item">
+                    <a data-toggle="tab" class="nav-link" href="#biz">Business</a>
+                </li>
+                <li class="nav-item">
+                    <a data-toggle="tab" class="nav-link" href="#design">Design</a>
+                </li>
+            </ul>
 
-                <label>
-                    <h2 class="col">Manager Home Page</h2>
-                </label>
-
-            </div>
-
-            <div class="row">
-                <ul class="nav nav-tabs col-36">
-                    <li class="nav-item">
-                        <a data-toggle="tab" class="nav-link" href="#com">Computing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a data-toggle="tab" class="nav-link" href="#biz">Business</a>
-                    </li>
-                    <li class="nav-item">
-                        <a data-toggle="tab" class="nav-link" href="#design">Design</a>
-                    </li>
-                </ul>
-
-                <!--Faculty 1 -->
-                <div class="tab-content col-36 ">
-                    <div id="com" class="tab-pane fade container ">
-                        <div class="row justify-content-between">
-                            <div class="info-boxy col-36">
-                                <div class="card-body row">
-                                    <label class="col-36">
-                                        <h3 class="card-title">Faculty of Computing</h3>
-                                    </label>
-                                    <div class="col-36 mb-3">
-                                        <button type="button" class="btn btn-primary zipDownload1">
-                                            <span>Download all published submission</span>
-                                        </button>
-                                    </div>
-                                    <div class="long-text col-36">
-                                        <table class="card-text table">
-                                            <thead>
-                                                <tr>
-                                                    <th>#</th>
-                                                    <th>User ID</th>
-                                                    <th>Submit date</th>
-                                                    <th>Submission</th>
-                                                    <th>Publication status</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody id="comTable">
-                                                <?php
-                                                getSubmission($conn, 1);
-                                                ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
+            <!--Faculty 1 -->
+            <div class="tab-content col-36 ">
+                <div id="com" class="tab-pane fade container ">
+                    <div class="row justify-content-between">
+                        <div class="info-boxy col-36">
+                            <div class="card-body row">
+                                <label class="col-36">
+                                    <h3 class="card-title">Faculty of Computing</h3>
+                                </label>
+                                <div class="col-36 mb-3">
+                                    <button type="button" class="btn btn-primary zipDownload1">
+                                        <span>Download all published submission</span>
+                                    </button>
+                                </div>
+                                <div class="long-text col-36">
+                                    <table class="card-text table">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>User ID</th>
+                                                <th>Submit date</th>
+                                                <th>Submission</th>
+                                                <th>Publication status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="comTable">
+                                            <?php
+                                            getSubmission($conn, 1);
+                                            ?>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <!--Faculty 2-->
-                    <div id="biz" class="tab-pane fade container">
-                        <div class="row justify-content-between">
-                            <div class="info-box col-36">
-                                <div class="card-body row">
-                                    <label class="col-36">
-                                        <h3 class="card-title">Faculty of Business</h3>
-                                    </label>
-                                    <div class="col-36 mb-3">
-                                        <button type="button" class="btn btn-primary zipDownload2">
-                                            <span>Download all published submission</span>
-                                        </button>
-                                    </div>
-                                    <div class="long-text col-36">
-                                        <table class="card-text table">
-                                            <thead>
-                                                <tr>
-                                                    <th>#</th>
-                                                    <th>User ID</th>
-                                                    <th>Submit date</th>
-                                                    <th>Submission</th>
-                                                    <th>Publication status</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody id="bizTable">
-                                                <?php
-                                                getSubmission($conn, 2);
-                                                ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
+                <!--Faculty 2-->
+                <div id="biz" class="tab-pane fade container">
+                    <div class="row justify-content-between">
+                        <div class="info-box col-36">
+                            <div class="card-body row">
+                                <label class="col-36">
+                                    <h3 class="card-title">Faculty of Business</h3>
+                                </label>
+                                <div class="col-36 mb-3">
+                                    <button type="button" class="btn btn-primary zipDownload2">
+                                        <span>Download all published submission</span>
+                                    </button>
+                                </div>
+                                <div class="long-text col-36">
+                                    <table class="card-text table">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>User ID</th>
+                                                <th>Submit date</th>
+                                                <th>Submission</th>
+                                                <th>Publication status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="bizTable">
+                                            <?php
+                                            getSubmission($conn, 2);
+                                            ?>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <!--Faculty 3-->
-                    <div id="design" class="tab-pane fade container">
-                        <div class="row justify-content-between">
-                            <div class="info-box col-36">
-                                <div class="card-body row">
-                                    <label class="col-36">
-                                        <h3 class="card-title">Faculty of Design</h3>
-                                    </label>
-                                    <div class="col-36 mb-3">
-                                        <button type="button" class="btn btn-primary zipDownload3">
-                                            <span>Download all published submission</span>
-                                        </button>
-                                    </div>
-                                    <div class="long-text col-36">
-                                        <table class="card-text table">
-                                            <thead>
-                                                <tr>
-                                                    <th>#</th>
-                                                    <th>User ID</th>
-                                                    <th>Submit date</th>
-                                                    <th>Submission</th>
-                                                    <th>Publication status</th>
-                                                </tr>
+                <!--Faculty 3-->
+                <div id="design" class="tab-pane fade container">
+                    <div class="row justify-content-between">
+                        <div class="info-box col-36">
+                            <div class="card-body row">
+                                <label class="col-36">
+                                    <h3 class="card-title">Faculty of Design</h3>
+                                </label>
+                                <div class="col-36 mb-3">
+                                    <button type="button" class="btn btn-primary zipDownload3">
+                                        <span>Download all published submission</span>
+                                    </button>
+                                </div>
+                                <div class="long-text col-36">
+                                    <table class="card-text table">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>User ID</th>
+                                                <th>Submit date</th>
+                                                <th>Submission</th>
+                                                <th>Publication status</th>
+                                            </tr>
 
-                                            </thead>
-                                            <tbody id="dsgTable">
-                                                <?php
-                                                getSubmission($conn, 3);
-                                                ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
+                                        </thead>
+                                        <tbody id="dsgTable">
+                                            <?php
+                                            getSubmission($conn, 3);
+                                            ?>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
@@ -231,11 +232,12 @@ function getSubmission($conn, $facultyId)
             </div>
         </div>
     </div>
-    <!--Default footer-->
-    <div id="footer" class="header-footer">
-        <span>@ 2019 2-1640 group.</span>
-    </div>
-    <script src="js/getRowID.js"></script>
+</div>
+<!--Default footer-->
+<div id="footer" class="header-footer">
+    <span>@ 2019 2-1640 group.</span>
+</div>
+<script src="js/getRowID.js"></script>
 </body>
 
 </html>
